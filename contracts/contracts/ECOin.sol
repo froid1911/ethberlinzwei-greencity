06a332a06512172434afbcca7ff4b63a2561b0c7
Christoph Niemann <christoph.niemann@maibornwolff.de>
2019-08-24 10:15:26 +0200

    Rename GCToken to ECOin

--- contracts/contracts/ECOin.sol
+++ contracts/contracts/ECOin.sol
@@ -4,7 +4,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
 
-contract GCToken is ERC20, ERC20Detailed, ERC20Mintable {
-  constructor() ERC20Detailed("Greencity token", "GCT", 18) public {
+contract ECOin is ERC20, ERC20Detailed, ERC20Mintable {
+  constructor() ERC20Detailed("Greencity token ECOin", "ECO", 18) public {
   }
 }
--- contracts/contracts/Greencity.sol
+++ contracts/contracts/Greencity.sol
@@ -11,25 +11,45 @@ contract Greencity {
   }
 
   struct Challenge {
-    uint id;
     ChallengeStates state;
     address person;
+    bytes ipfsLink;
+    uint prize;
+    int confirmed;
   }
 
   mapping (address => Challenge) challenges;
-  uint nextId;
 
-  event ChallengeStarted(uint id);
+  event ChallengeStarted(address challenger);
+  event ChallengeStopped(address challenger, uint prize, bytes ipfsLink);
 
   constructor() public {
-    nextId = 1;
   }
 
-  function startChallenge() public returns (uint id) {
-    id = nextId;
-    challenges[msg.sender] = Challenge(id, ChallengeStates.STARTED, msg.sender);
-    nextId++;
-    emit ChallengeStarted(id);
+  function startChallenge() public {
+    challenges[msg.sender] = Challenge(ChallengeStates.STARTED, msg.sender, "", 0, 0);
+    emit ChallengeStarted(msg.sender);
+  }
+
+  function stopChallenge(uint _prize, bytes _ipfsLink) public {
+    require (challenges[msg.sender] != null, "No active challenge");
+    requrei (challenges[msg.sender].state == ChallengeStates.STARTED, "Challenge not in STARTED state");
+    challenges[msg.sender].state = ChallengeStates.FINISHED;
+    challenges[msg.sender].prize = _prize;
+    challenges[msg.sender].ipfsLink = _ipfsLink;
+    challenges[msg.sender].confirmed = 1;
+
+    emit ChallengeStopped(msg.sender, challenges[msg.sender].prize, challenges[msg.sender].ipfsLink)
+  }
+
+  function confirmChallange(address challenger) public returns (uint) {
+    challenges[msg.sender].confirmed += 1;
+    return challenges[msg.sender].confirmed;
+  }
+
+  function rejectChallenge(address challenger) public returns (uint) {
+    challenges[msg.sender].confirmed -= 1;
+    return challenges[msg.sender].confirmed;
   }
 
 }
