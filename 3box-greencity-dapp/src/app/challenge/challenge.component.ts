34c29bcb830d47fed3e4f167c7175d26ce908104
Frank Polster <frank.polster@maibornwolff.de>
2019-08-25 01:55:23 +0200

    zwischenstand

--- 3box-greencity-dapp/src/app/challenge/challenge.component.ts
+++ 3box-greencity-dapp/src/app/challenge/challenge.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from "@angular/core";
 import { NavParams, ModalController } from "@ionic/angular";
 import { EthereumService } from "../ethereum.service";
+import { BoxService } from "../box.service";
+import { Web3Service } from "../web3.service";
+import TripData from "./../../../goodTrip.json";
 
 @Component({
   selector: "app-challenge",
@@ -14,7 +17,9 @@ export class ChallengeComponent implements OnInit {
   constructor(
     params: NavParams,
     private modal: ModalController,
-    private ethereum: EthereumService
+    private ethereum: EthereumService,
+    private box: BoxService,
+    private web3Service: Web3Service
   ) {
     this.challenge = params.get("challenge");
   }
@@ -23,12 +28,15 @@ export class ChallengeComponent implements OnInit {
 
   start() {
     this.alreadyStarted = true;
-    this.ethereum.start();
+
+    this.ethereum.start("0x3840Da83b4EC0CFEcE8acBcf86CA5196B086e605");
     // Push Data to 3Box Thread
   }
 
   stop() {
+    this.ethereum.stop(500);
     this.alreadyStarted = false;
+    this.box.pushData(TripData);
     // Push Data to IPFS
     // Save Data in 3Box Storage
     this.modal.dismiss();
--- 3box-greencity-dapp/src/app/web3.service.ts
+++ 3box-greencity-dapp/src/app/web3.service.ts
@@ -7,11 +7,13 @@ import Web3 from "web3";
 export class Web3Service {
   web3: Web3;
   chainId: any;
+  accounts: any;
   constructor() {}
 
   init() {
     return new Promise(resolve => {
       window["ethereum"].enable().then(async accounts => {
+        this.accounts = accounts;
         this.web3 = new Web3(window["ethereum"]);
         console.log(this.web3);
         this.chainId = await this.web3.eth.net.getId();
@@ -26,5 +28,9 @@ export class Web3Service {
 
   getChainId() {
     return this.chainId;
+  }
+
+  getAccount() {
+    return this.accounts[0];
   }
 }
